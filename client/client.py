cc241a351bca4e61367dcb5d2b6f3c2008da4a3a
gmolki <gerardmo98@gmail.com>
2020-02-28 19:53:25 +0100

    Added arguments parser to specify input file and refactor of code

--- client/client.py
+++ client/client.py
@@ -1,15 +1,55 @@
 import csv
 import requests
+import sys
+import getopt
 
-with open('sample.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    nReq = 0
-    for row in csv_reader:
-        operation = {'value1': row[0], 'operator': row[1], 'value2': row[2]}
-        print 'Request no. ', nReq
-        print '\tSending:\t', operation
-        serverResponse = requests.post(
-            url='http://127.0.0.1:5000/', json=operation)
-        resJson = serverResponse.json()
-        nReq += 1
-        print '\tResponse:\t', serverResponse.json()
+
+def sendRequest(operation):
+    print '\tSending\t:', operation
+    serverResponse = requests.post(
+        url='http://127.0.0.1:5000/', json=operation)
+
+    resJson = serverResponse.json()
+    print '\tResponse\t:', serverResponse.json()
+
+
+def readCsv(file):
+    with open('sample.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        nReq = 0  # Counter of requests sent to the server
+        for row in csv_reader:
+            # Reads every row of the csv file and sends the data to the
+            # server to get the operation result
+            operation = {'value1': row[0],
+                         'operator': row[1], 'value2': row[2]}
+
+            nReq += 1
+            print 'Request no. ', nReq
+            sendRequest(operation)
+
+
+def main(argv):
+
+    try:
+        # Specify what options and arguments are accepted by the program
+        unixOptions = "hi:"
+        gnuOptions = ["help", "input="]
+        opts, args = getopt.getopt(argv, unixOptions, gnuOptions)
+    except getopt.error as err:
+        # Print error information and exit
+        print(str(err))
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            # Help option
+            print "\nUsage:\npython client.py [option] [arg]\n\nOptions and arguments:\n-h\t: print this help message and exit (also --help)\n-i\t: csv input file to read the operations and calculate them,\n\t  needed to specify the extension (also --input)\n"
+            sys.exit(0)
+        elif opt in ("-i", "--input"):
+            # Input option
+            print "input file\t: ", arg
+            readCsv(arg)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
