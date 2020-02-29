b5e74c6ce477ff3d0520b363aa8f66b854116479
gmolki <gerardmo98@gmail.com>
2020-02-29 18:44:46 +0100

    Server log added

--- server/server.py
+++ server/server.py
@@ -1,4 +1,16 @@
 from flask import Flask, jsonify, request
+import logging
+
+app = Flask(__name__)
+
+
+def log(data):
+    logging.basicConfig(filename='./log.txt',
+                        filemode='w',
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S',
+                        level=logging.INFO)
+    logging.info(data)
 
 
 def calculate(value1, operator, value2):
@@ -12,16 +24,21 @@ def calculate(value1, operator, value2):
         return value1 / value2
 
 
-app = Flask(__name__)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    logString = ''
     if request.method == 'POST':
         userSent = request.get_json()
-        print '\tClient sent:', userSent
+        # print '\tClient sent:', userSent
+        logString = 'Operation:\t ' + \
+            userSent['value1'] + ' ' + userSent['operator'] + ' ' + \
+            userSent['value2']
+        log(logString)
         solution = calculate(
             int(userSent['value1']), userSent['operator'], int(userSent['value2']))
+
+        logString = 'Solution:\t ' + str(solution)
+        log(logString)
         return jsonify({"sent": userSent, 'solution': solution})
     elif request.method == 'GET':
         return '<h1>Hello<h1>'
